92761a7f276e2ec31a20f6aa3104a2c2ef7b0e1f
Sal Dragone <104164651+sdragone01@users.noreply.github.com>
2022-09-30 11:27:59 -0400

    fixed customer form

--- src/components/Forms/CustomerForm.js
+++ src/components/Forms/CustomerForm.js
@@ -1,183 +1,144 @@
-import React from "react";
-import {
-  TextField,
-  FormControl,
-  InputLabel,
-  Select,
-  MenuItem,
-  Box,
-} from "@mui/material";
-import fire from "../../config/Fire";
-import { useState, Component } from "react";
+import React, { Component } from "react";
+import Fire from "../../config/Fire";
 
 export default class CustomerForm extends Component {
+
   state = {
-    CompanyName: "",
     name: "",
-    email: "",
-    phone: "",
     address: "",
     city: "",
     state: "",
     zip: "",
-    country: "",
-    previousOrders: [],
+    phone: "",
+    email: "",
+    contact: "",
+    notes: "",
   };
 
-  handleChange = (event) => {
-    this.setState({ [event.target.value]: event.target.value });
+  handleChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+
   };
 
-  handleSubmit = (event) => {
-    event.preventDefault();
-    const db = fire.firestore();
+  handleSubmit = (e) => {
+
+    e.preventDefault();
+    const db = Fire.firestore();
     db.collection("customers").add({
-      CompanyName: this.state.CompanyName,
       name: this.state.name,
-      email: this.state.email,
-      phone: this.state.phone,
       address: this.state.address,
       city: this.state.city,
       state: this.state.state,
       zip: this.state.zip,
-      country: this.state.country,
-      previousOrders: this.state.previousOrders,
+      phone: this.state.phone,
+      email: this.state.email,
+      contact: this.state.contact,
+      notes: this.state.notes,
     });
     this.setState({
-      CompanyName: "",
       name: "",
-      email: "",
-      phone: "",
       address: "",
       city: "",
       state: "",
       zip: "",
-      country: "",
-      previousOrders: [],
+      phone: "",
+      email: "",
+      contact: "",
+      notes: "",
     });
+
   };
 
   render() {
+
     return (
       <div>
-        <form className="form-container">
-          <TextField
-            id="CompanyName"
-            label="Company Name"
-            variant="outlined"
-            value={this.state.CompanyName}
-            onChange={this.handleChange}
-          />
-          <TextField
-            id="name"
-            label="Name"
-            variant="outlined"
-            value={this.state.name}
-            onChange={this.handleChange}
-          />
-          <TextField
-            id="email"
-            label="Email"
-            variant="outlined"
-            value={this.state.email}
-            onChange={this.handleChange}
-          />
-          <TextField
-            id="phone"
-            label="Phone"
-            variant="outlined"
-            value={this.state.phone}
-            onChange={this.handleChange}
-          />
-          <TextField
-            id="address"
-            label="Address"
-            variant="outlined"
-            value={this.state.address}
-            onChange={this.handleChange}
-          />
-          <TextField
-            id="city"
-            label="City"
-            variant="outlined"
-            value={this.state.city}
-            onChange={this.handleChange}
-          />
-          <Select
-            id="state"
-            label="State"
-            variant="outlined"
-            value={this.state.state}
-            onChange={this.handleChange}
-          >
-            <MenuItem value="AL">Alabama</MenuItem>
-            <MenuItem value="AK">Alaska</MenuItem>
-            <MenuItem value="AZ">Arizona</MenuItem>
-            <MenuItem value="AR">Arkansas</MenuItem>
-            <MenuItem value="CA">California</MenuItem>
-            <MenuItem value="CO">Colorado</MenuItem>
-            <MenuItem value="CT">Connecticut</MenuItem>
-            <MenuItem value="DE">Delaware</MenuItem>
-            <MenuItem value="DC">District Of Columbia</MenuItem>
-            <MenuItem value="FL">Florida</MenuItem>
-            <MenuItem value="GA">Georgia</MenuItem>
-            <MenuItem value="HI">Hawaii</MenuItem>
-            <MenuItem value="ID">Idaho</MenuItem>
-            <MenuItem value="IL">Illinois</MenuItem>
-            <MenuItem value="IN">Indiana</MenuItem>
-            <MenuItem value="IA">Iowa</MenuItem>
-            <MenuItem value="KS">Kansas</MenuItem>
-            <MenuItem value="KY">Kentucky</MenuItem>
-            <MenuItem value="LA">Louisiana</MenuItem>
-            <MenuItem value="ME">Maine</MenuItem>
-            <MenuItem value="MD">Maryland</MenuItem>
-            <MenuItem value="MA">Massachusetts</MenuItem>
-            <MenuItem value="MI">Michigan</MenuItem>
-            <MenuItem value="MN">Minnesota</MenuItem>
-            <MenuItem value="MS">Mississippi</MenuItem>
-            <MenuItem value="MO">Missouri</MenuItem>
-            <MenuItem value="MT">Montana</MenuItem>
-            <MenuItem value="NE">Nebraska</MenuItem>
-            <MenuItem value="NV">Nevada</MenuItem>
-            <MenuItem value="NH">New Hampshire</MenuItem>
-            <MenuItem value="NJ">New Jersey</MenuItem>
-            <MenuItem value="NM">New Mexico</MenuItem>
-            <MenuItem value="NY">New York</MenuItem>
-            <MenuItem value="NC">North Carolina</MenuItem>
-            <MenuItem value="ND">North Dakota</MenuItem>
-            <MenuItem value="OH">Ohio</MenuItem>
-            <MenuItem value="OK">Oklahoma</MenuItem>
-            <MenuItem value="OR">Oregon</MenuItem>
-            <MenuItem value="PA">Pennsylvania</MenuItem>
-            <MenuItem value="RI">Rhode Island</MenuItem>
-            <MenuItem value="SC">South Carolina</MenuItem>
-            <MenuItem value="SD">South Dakota</MenuItem>
-            <MenuItem value="TN">Tennessee</MenuItem>
-            <MenuItem value="TX">Texas</MenuItem>
-            <MenuItem value="UT">Utah</MenuItem>
-            <MenuItem value="VT">Vermont</MenuItem>
-            <MenuItem value="VA">Virginia</MenuItem>
-            <MenuItem value="WA">Washington</MenuItem>
-            <MenuItem value="WV">West Virginia</MenuItem>
-            <MenuItem value="WI">Wisconsin</MenuItem>
-            <MenuItem value="WY">Wyoming</MenuItem>
-          </Select>
-          <TextField
-            id="zip"
-            label="Zip"
-            variant="outlined"
-            value={this.state.zip}
-            onChange={this.handleChange}
-          />
-          <TextField
-            id="country"
-            label="Country"
-            variant="outlined"
-            value={this.state.country}
-            onChange={this.handleChange}
-          />
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Name:
+            <input
+              type="text"
+              name="name"
+              value={this.state.name}
+              onChange={this.handleChange}
+            />
+          </label>
+          <label>
+            Address:
+            <input
+              type="text"
+              name="address"
+              value={this.state.address}
+              onChange={this.handleChange}
+            />
+          </label>
+          <label>
+            City:
+            <input
+              type="text"
+              name="city"
+              value={this.state.city}
+              onChange={this.handleChange}
+            />
+          </label>
+          <label>
+            State:
+            <input
+              type="text"
+              name="state"
+              value={this.state.state}
+              onChange={this.handleChange}
+            />
+          </label>
+          <label>
+            Zip:
+            <input
+              type="text"
+              name="zip"
+              value={this.state.zip}
+              onChange={this.handleChange}
+            />
+          </label>
+          <label>
+            Phone:
+            <input
+              type="text"
+              name="phone"
+              value={this.state.phone}
+              onChange={this.handleChange}
+            />
+          </label>
+          <label>
+            Email:
+            <input
+              type="text"
+              name="email"
+              value={this.state.email}
+              onChange={this.handleChange}
+            />
+          </label>
+          <label>
+            Contact:
+            <input
+              type="text"
+              name="contact"
+              value={this.state.contact}
+              onChange={this.handleChange}
+            />
+          </label>
+          <label>
+            Notes:
+            <input
+              type="text"
+              name="notes"
+              value={this.state.notes}
+              onChange={this.handleChange}
+            />
+          </label>
+          <input type="submit" value="Submit" />
         </form>
       </div>
     );
+
   }
-}
+} 
---
+++ src/components/Forms/NewJobForm copy.js
@@ -0,0 +1,121 @@
+import { bool } from 'prop-types';
+import React, { Component } from 'react';
+import Fire from '../../config/Fire';
+import DatePicker from './DatePicker/DatePicker';
+
+//MUI
+
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import { Button } from '@mui/material';
+
+
+
+
+
+
+export default class NewJobForm extends Component {
+    state = {
+        po: '',
+        deliveryMethod: bool,
+        created: {},
+        prodDue: [],
+        customerDue: '',
+        customer: '',
+        job: '',
+    }
+
+    handleChange = (e) => {
+        this.setState({ [e.target.name]: e.target.value });
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        const db = Fire.firestore();
+        db.collection('jobs').add({
+            po: this.state.po,
+            deliveryMethod: this.state.deliveryMethod,
+            created: this.state.created,
+            prodDue: this.state.prodDue,
+            customerDue: this.state.customerDue,
+            customer: this.state.customer,
+            job: this.state.job,
+        });
+        this.setState({
+            po: '',
+            deliveryMethod: bool,
+            created: '',
+            prodDue: '',
+            customerDue: '',
+            customer: '',
+            job: '',
+        });
+    }
+
+    render() {
+        return (
+            <Box
+                component="form"
+                sx={{
+                    '& > :not(style)': { m: 1, width: '25ch' },
+                }}
+                noValidate
+                autoComplete="off"
+                onSubmit={this.handleSubmit}
+            >
+                <TextField
+                    id="outlined-basic"
+                    label="PO"
+                    variant="outlined"
+                    name="po"
+                    value={this.state.po}
+                    onChange={this.handleChange}
+                />
+                <FormControl >
+                    <InputLabel id="demo-simple-select-label">Delivery Method</InputLabel>
+                    <Select
+                        labelId="demo-simple-select-label"
+                        id="demo-simple-select"
+                        value={this.state.deliveryMethod}
+                        label="Delivery Method"
+                        name="deliveryMethod"
+                        onChange={this.handleChange}
+                    >
+                        <MenuItem value={true}>Delivery</MenuItem>
+                        <MenuItem value={false}>Pickup</MenuItem>
+                    </Select>
+                </FormControl>
+                <DatePicker
+                    label="Created"
+                    name="created"
+                    value={this.state.created}
+                    onChange={this.handleChange}
+                />
+                <DatePicker
+                    label="Production Due"
+                    name="prodDue"
+                    value={this.state.prodDue}
+                    onChange={this.handleChange}
+                />
+                <DatePicker
+                    label="Customer Due"
+                    name="customerDue"
+                    value={this.state.customerDue}
+                    onChange={this.handleChange}
+                />
+
+                <Button variant="contained" type="submit">Submit</Button>
+
+
+            </Box>
+        )
+
+    }
+
+
+
+}
