2b18abd60c146aaa415f2bb29b99669059e46a06
Edgie Manzo <104868823+emanzo14@users.noreply.github.com>
2022-09-30 11:12:51 -0400

    adding customer form

--- src/pages/CRM/CustomersMain.jsx
+++ src/pages/CRM/CustomersMain.jsx
@@ -1,8 +1,11 @@
-import './CRM.css'
+import "./CRM.css";
+import CustomerForm from "../../components/Forms/CustomerForm";
+
 export default function CustomersMain() {
-    return (
-        <div className="customers-main">
-            <h1>CustomersMain</h1>
-        </div>
-    )
+  return (
+    <div className="customers-main">
+      <h1>CustomersMain</h1>
+      <CustomerForm />
+    </div>
+  );
 }
