fec0fbae05e35b7b9b0998393612171ff0667b84
Sal Dragone <104164651+sdragone01@users.noreply.github.com>
2022-10-05 18:33:56 -0400

    delete button on table

--- src/pages/CRM/CustomersMain2.jsx
+++ src/pages/CRM/CustomersMain2.jsx
@@ -1,17 +1,23 @@
+import DeleteIcon from '@mui/icons-material/Delete';
 
 import { DataGrid } from "@mui/x-data-grid";
 import fire from "../../config/Fire";
-import { getDatabase, ref, set, onValue, push, child, get, query } from "firebase/database";
+import { getDatabase, ref, set, onValue, push, child, get, query, remove } from "firebase/database";
 import React from "react";
+
 const db = getDatabase();
+
 
 export default class CustomersMain extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       customerArray: [],
+      selectionModel: [],
+
     };
   }
+
 
 
 
@@ -33,12 +39,17 @@ export default class CustomersMain extends React.Component {
     });
   }
 
+  onSelect = (selectionModel) => {
+    this.setState({ selectionModel });
+  };
+
+
 
   render() {
     const columns = [
       { field: "id", headerName: "ID", width: 90 },
       {
-        field: "fullName",
+        field: "name",
         headerName: "Full Name",
         width: 150,
         editable: true,
@@ -67,13 +78,28 @@ export default class CustomersMain extends React.Component {
     ];
 
     return (
-      <div style={{ height: 400, width: "100%" }}>
+      <div style={{ height: 400, width: "90%" }}>
+
         <DataGrid
+
           rows={this.state.customerArray}
           columns={columns}
           pageSize={5}
           checkboxSelection
+          disableSelectionOnClick
+          onSelectionModelChange={(checkboxSelection) => {
+            this.setState({ checkboxSelection });
+          }}
         />
+        <button onClick={() => {
+          const dbRef = ref(db, "Customers");
+          for (let i = 0; i < this.state.checkboxSelection.length; i++) {
+            remove(child(dbRef, this.state.checkboxSelection[i]));
+          }
+
+        }}>Delete</button>
+
+
       </div>
     );
   }
