0fec6f98a6f64006f8e42015b25bfacdae6cc10e
Sal Dragone <104164651+sdragone01@users.noreply.github.com>
2022-10-05 16:49:54 -0400

    customer data table

--- src/pages/CRM/CustomersMain2.jsx
+++ src/pages/CRM/CustomersMain2.jsx
@@ -1,36 +1,77 @@
-import { getDatabase, ref, set, onValue, push, child, get, query } from "firebase/database";
+import { getDatabase, ref, set, onValue, push, child, get, query, orderByChild } from "firebase/database";
 
 
 
+import { useState, useEffect, useMemo } from "react";
 
-
-
+import { DataGrid } from "@mui/x-data-grid";
 export default function CustomersMain2() {
 
-  const dbRef = ref(getDatabase());
-  get(child(dbRef, 'Customers')).then((snapshot) => {
-    if (snapshot.exists()) {
-      console.log(snapshot.val());
-    } else {
-      console.log("No data available");
-    }
-  }).catch((error) => {
-    console.error(error);
+  const db = getDatabase();
+  const [customerArray, setCustomerArray] = useState([]);
+
+  const customerData = query(ref(db, "Customers"), orderByChild("fullName"));
+  onValue(customerData, (snapshot) => {
+    snapshot.forEach((childSnapshot) => {
+      const childKey = childSnapshot.key;
+      const childData = childSnapshot.val();
+
+      customerArray.push(childData);
+    });
   });
 
+  console.log(customerArray);
 
+  const customers = useMemo(() => {
+    return customerArray.map((customer, index) => {
+      return {
+        id: index,
+        name: customer.name,
+        email: customer.email,
+        phone: customer.phone,
+        address: customer.address,
+      };
+    });
+  }, [customerArray]);
 
+  const columns = [
+    { field: "id", headerName: "ID", width: 90 },
 
+    {
+      field: "name",
+      headerName: "Name",
+      width: 150,
+      editable: true,
+    },
+    {
+      field: "email",
+      headerName: "Email",
+      width: 150,
+      editable: true,
+    },
+    {
+      field: "phone",
+      headerName: "Phone",
+      type: "number",
+      width: 110,
+      editable: true,
+    },
+    {
+      field: "address",
+      headerName: "Address",
+      description: "This column has a value getter and is not sortable.",
+      sortable: false,
+      width: 160,
+      editable: true,
+    },
+  ];
 
+  const rows = customers;
 
 
   return (
-    <div className="customers-main">
-
-
-
-      <h1>CustomersMain2</h1>
-
+    <div style={{ height: 400, width: "100%" }}>
+      <DataGrid rows={rows} columns={columns} pageSize={5} checkboxSelection />
     </div>
   );
 }
